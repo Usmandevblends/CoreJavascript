cafc37d20f3a2bc00a2c6ee20852209358d1f6de
Usmandevblends <167763547+Usmandevblends@users.noreply.github.com>
2024-09-23 19:57:37 +0500

    "Commented out code in harry.js and updated index.html with changes to title, favicon, and script tags."

---
+++ apnacollage/function.js
@@ -0,0 +1,118 @@
+// function sum(a, b) {
+//   console.log(a + b);
+// }
+
+// function calculator(a, b, sumCallback) {
+//   sumCallback(a, b);
+// }
+
+// calculator(1, 2, sum);
+
+// Asyschronous callbacks
+// const hello = () => {
+//   console.log("Hi");
+// };
+// setTimeout(hello, 3000);
+
+// function greet(name, callback) {
+//     console.log('Hi' + ' ' + name);
+//     callback();
+// }
+
+// // callback function
+// function callMe() {
+//     console.log('I am callback function');
+// }
+
+// // passing function as an argument
+// greet('Peter', callMe);
+
+// promises
+
+// const getPromise = () => {
+//     return new Promise((resolve, reject) => {
+//         console.log("I am a Promise");
+//         resolve("success");
+//         // reject("error");
+//     });
+// };
+
+// let promise = getPromise();
+// promise.then((res) => {
+//     console.log("Promise resolved", res);
+// });
+
+// promise.catch((err) => {
+//     console.log("rejected", err);
+// });
+
+// function asyncFunc() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(()=> {
+//             console.log('some data1')
+//         },3000)
+//     });
+// }
+
+// call backs funcation examples
+
+// function greet(name, callback) {
+//     console.log("Hello, " + name);
+//     callback();
+// }
+// greet("John", sayGoodbye);
+// function sayGoodbye() {
+//     console.log("Goodbye!");
+// }
+
+// Question 1
+
+// function greetUser(name, callback) {
+//     return callback(name);
+//   }
+
+//   function greetingMessage(name) {
+//     return "Hello, " + name + "!";
+//   }
+
+//   const message = greetUser("John", greetingMessage);
+//   console.log(message);
+
+// Q2
+
+// function calculate(num1, num2, callback) {
+//   return callback(num1, num2);
+// }
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function subtract(a, b) {
+//   return a - b;
+// }
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// console.log(calculate(5, 3, add));
+// console.log(calculate(5, 3, subtract));
+// console.log(calculate(5, 3, multiply));
+
+// QNo
+
+function performAsyncOperation(callback) {
+  console.log("Fetching data...");
+
+  setTimeout(function () {
+    const data = { name: "Sample Data" };
+    callback(data);
+  }, 2000);
+}
+
+function handleData(data) {
+  console.log("Data received:", data);
+}
+
+performAsyncOperation(handleData);
--- harry.js
+++ harry.js
@@ -115,15 +115,15 @@
 //  Asynchronous funcation simple example
 
 
-function myDisplayer(something) {
-    document.getElementById("demo").innerHTML = something;
-  }
+// function myDisplayer(something) {
+//     document.getElementById("demo").innerHTML = something;
+//   }
   
-  function myCalculator(num1, num2, myCallback) {
-    let sum = num1 + num2;
-    myCallback(sum);
-  }
+//   function myCalculator(num1, num2, myCallback) {
+//     let sum = num1 + num2;
+//     myCallback(sum);
+//   }
   
-  myCalculator(5, 5, myDisplayer);
+//   myCalculator(5, 5, myDisplayer);
 
-  
+
